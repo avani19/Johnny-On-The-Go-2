1eccfdd6036a3442a571fc751e8fddd9dae33ba7
Ryan Seery <Ryan Seery>
2017-01-14 11:42:09 -0500

    working on geoloaction

--- myapp/client/main.js
+++ myapp/client/main.js
@@ -4,5 +4,5 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 
 Meteor.startup(function() {
-  GoogleMaps.load();
+  GoogleMaps.load( { v: '3', key: 'AIzaSyDcwH9pW1RsFCRy50OpynEc_YyhPg7icZ0', libraries: 'geometry,places'});
 });
