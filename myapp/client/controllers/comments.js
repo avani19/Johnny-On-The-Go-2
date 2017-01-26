02fe5ba6dc110a82848a63f7b32bffe5fa84ac20
avani19 <avaniipatels5@gmail.com>
2017-01-26 14:53:17 -0500

    Chnges in CSS

--- myapp/client/controllers/comments.js
+++ myapp/client/controllers/comments.js
@@ -1,12 +1,33 @@
+// Template.comments.events = {
+//   'click #submit' : function(event){
+//     event.preventDefault();
+//     var title = $('#title').val();
+//     var body = $('#body').val();
+//     comments.insert({
+//       title: title,
+//       body: body
+//     });
+//     $('#title, #body').val('');
+//   }
+// }
 Template.comments.events = {
   'click #submit' : function(event){
     event.preventDefault();
-    var title = $('#title').val();
-    var body = $('#body').val();
-    comments.insert({
-      title: title,
-      body: body
+    var paidOrFree = $('#paidOrFree').val();
+    var timesOpen = $('#timesOpen').val();
+    var publicBusiness = $('#publicBusiness').val();
+    var comments = $('#comments').val();
+    Markers.insert({
+      paidOrFree: paidOrFree,
+      timesOpen: timesOpen,
+      publicBusiness: publicBusiness,
+      comments: comments,
+      users: {
+        _id: Meteor.userId(),
+        email: Meteor.user().emails[0].address
+      }
     });
-    $('#title, #body').val('');
+    $('#paidOrFree, #timesOpen, #publicBusiness, #comments').val('');
+    $('#imageModal').closeModal();
   }
 }
--- myapp/client/controllers/mapForm.js
+++ myapp/client/controllers/mapForm.js
@@ -1,3 +1,15 @@
+// Template.mapForm.events({
+//   'submit #mapForm': function(event){
+//     event.preventDefault();
+//     console.log("User Position is Stored as: "+JSON.stringify(Session.get('position')));
+//     var position = Session.get('position');
+//     Markers.insert({
+//       lat: position.lat,
+//       lng: position.lng,
+//       image: './marker.png',
+//     });
+//   }
+// });
 Template.mapForm.events({
   'submit #mapForm': function(event){
     event.preventDefault();
@@ -7,6 +19,10 @@ Template.mapForm.events({
       lat: position.lat,
       lng: position.lng,
       image: './marker.png',
+      // paidOrFree: paidOrFree,
+      // timesOpen: timesOpen,
+      // publicBusiness: publicBusiness,
+      // comments: comments
     });
   }
 });
